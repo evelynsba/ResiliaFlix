6cccaa1c59a9cc1657c47a99746ca9eefa3d4f4d
Vitória Bernardino <vitorianbernardino@gmail.com>
2021-09-23 11:43:26 -0300

    correção de bug

--- src/js/recuperacaoDeSenha.js
+++ src/js/recuperacaoDeSenha.js
@@ -1,5 +1,19 @@
-botao = document.querySelector("#botao")
-botao.addEventListener("click", (event)=> {
-    event.preventDefault()
-    alert("Formulário Enviado")
-    })
+function validacaoEmail(field){
+    usuario = field.value.substring(0, field.value.indexOf("@"));
+    dominio = field.value.substring(field.value.indexOf("@") + 1, field.value.length);
+    if (
+        (usuario.length >= 1) &&
+        (dominio.length >= 3) &&
+        (usuario.search("@") == -1) &&
+        (dominio.search("@") == -1) &&
+        (usuario.search(" ") == -1) &&
+        (dominio.search(" ") == -1) &&
+        (dominio.search(".") != -1) &&
+        (dominio.indexOf(".") >= 1) &&
+        (dominio.lastIndexOf(".") < dominio.length - 1)) {
+        alert("E-mail enviado")
+        }
+    else {
+        alert("Digite novamente uma resposta valida")
+    } 
+}
