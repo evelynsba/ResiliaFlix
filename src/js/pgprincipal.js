c534b3438940d17aae22163d46c4100e4f23bf35
Rodrigo Aquino <rodrigovrvr@gmail.com>
2021-09-23 02:43:14 -0300

    couteudos da pgprincipal

--- src/js/pgprincipal.js
+++ src/js/pgprincipal.js
@@ -1,24 +1,31 @@
 $('.caroussel').slick({
-    centerMode: true,
-    centerPadding: '60px',
+    dots: true,
+    infinite: true,
+    speed: 300,
     slidesToShow: 3,
+    slidesToScroll: 4,
     responsive: [
       {
-        breakpoint: 768,
+        breakpoint: 1024,
         settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 3
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
         }
       },
       {
         breakpoint: 480,
         settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 1
+          slidesToShow: 1,
+          slidesToScroll: 1
         }
       }
     ]
